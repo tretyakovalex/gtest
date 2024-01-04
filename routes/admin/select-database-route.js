e53ee2959af5f1003dde9721c01ef95169f616f3
tretyakovalex <tretyakovalexmax@gmail.com>
2024-01-04 19:42:45 +0400

    savingdb to file

--- routes/admin/select-database-route.js
+++ routes/admin/select-database-route.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
 
 const { pool, setDatabaseName } = require('../../configs/mysql');
+
+const databaseConfigPath = path.join(__dirname, '../../configs/databaseConfig.json')
 
 router.post('/api/select-database', async (req, res) => {
     const newDatabaseName = req.body.databaseName;
 
     if (newDatabaseName) {
         console.log(newDatabaseName);
-        setDatabaseName(newDatabaseName);
+        // setDatabaseName(newDatabaseName);
+        fs.writeFileSync(databaseConfigPath, JSON.stringify({ 'selectedDatabase': newDatabaseName }));
         res.json({ success: true, message: 'Database selected successfully' });
     } else {
         res.status(400).json({ success: false, message: 'Invalid database name' });
